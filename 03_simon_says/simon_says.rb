833fb32751b9a5b8ef7cc5b45e8fdde8e2b8f589
arthur <arthur@techie.com>
2019-04-30 02:06:25 -0400

    simon_says.rb

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,45 @@
 #write your code here
+def echo(phrase)
+        phrase
+end
+
+def shout(phrase)
+        phrase.upcase
+end
+
+def repeat(phrase, amount = 1)
+        temp = phrase
+        (amount - 1).times do
+                phrase += " " + temp
+        end
+
+        if (amount == 1)
+                phrase = temp + " " + temp
+        end
+
+        phrase
+end
+
+def start_of_word(word, amount)
+        word[0,amount]
+end
+
+def first_word(string)
+        myarray = string.split(" ")
+        myarray[0]
+end
+
+def titleize(word)
+        myarray = word.split(" ")
+        myarray[0] = myarray[0].capitalize
+        i=0
+        myarray.length.times do
+                if (myarray[i] == "and" || myarray[i] == "over" || myarray[i] == "the")
+                        i +=1
+                else
+                        myarray[i] = myarray[i].capitalize
+                        i += 1
+                end
+        end
+        myarray.join(" ")  
+end
