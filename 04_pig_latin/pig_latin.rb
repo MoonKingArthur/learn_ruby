7050c771f028be6451473a31344b52f58b593b36
arthur <arthur@techie.com>
2019-05-01 03:22:13 -0400

    Solve half of pig_latin.rb

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
 #write your code here
+def translate(words)
+        words = words.split()
+        def vowel?
+                "a" || "e" || "i" || "o" || "u"
+        end
+
+        words.length.times do |i|
+                if (words[i][0] == vowel?)
+                        words[i] += "ay" 
+                elsif (words[i][0] && words[i][1] != vowel?)
+                        words[i] = words[i][2..(words[i].length - 1)] + words[i][0] + words[i][1] + "ay"
+                else
+                        words[i] = words[i][1..(words[i].length - 1)] + words[i][0] + "ay"
+                end
+        end
+        words.join(" ")
+end
