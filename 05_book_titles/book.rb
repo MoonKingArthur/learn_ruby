0cfc146c95c362648cb151b7312219426dc6e452
arthur <arthur@techie.com>
2019-05-01 17:16:41 -0400

    Solve half of book.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
 # write your code here
+        attr_reader :title
+
+        def title=(words) # "war and peace"
+                def keyword?
+                       ("and" || "in" || "the" || "of")
+                end
+
+                words = words.split(" ") # {"war","and","peace"}
+                words.length.times do |i|#3 times
+                        if (words[i] != keyword?)
+                                words[i] = words[i].capitalize
+                        else
+                                words[i] = words[i].downcase
+                        end
+                        i += 1
+                ends
+                @title = words.join(" ")
+        end
 end
